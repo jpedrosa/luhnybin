0201554f2ec6546b7b654b36115311ba57edbf16
Joao Pedrosa <joaopedrosa@gmail.com>
2011-11-19 20:18:13 -0300

    Created a mask for the Ruby version by avoiding regexp altogether.

--- luhn.rb
+++ luhn.rb
@@ -150,12 +150,63 @@ class Luhn
     end
   end
 
+  def mask_with_case s
+    masked = nil
+    i = 0
+    digit_count = 0
+    digit_offset = 0
+    digits = []
+    match_from = -1
+    max_len = 0
+    len = s.length
+    c = ''
+    while i < len
+      c = s[i]
+      case c
+      when '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+        match_from = i if match_from < 0
+        digit_count += 1
+        digits << c.to_i
+        max_len = digit_count - digit_offset
+        if max_len >= 14
+          found = test_it(digits, digit_offset, max_len)
+          if found
+            masked = s[0..-1] if not masked
+            j = i
+            while j >= match_from
+              case s[j]
+              when '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+                masked[j] = 'X'
+              end
+              j -= 1
+            end
+          end
+        end
+        if max_len >= 16
+          digit_offset += 1
+          match_from += 1 
+        end
+      when '-', ' '
+        # Keep going.
+      else
+        if digit_count > 0
+          digit_count = 0
+          digits = []
+          match_from = -1
+          digit_offset = 0
+        end
+      end
+      i += 1
+    end
+    masked || s
+  end
+
   def tap_stdin
     n_repeats = ARGV[0] ? ARGV[0].to_i : 1
     lines = STDIN.readlines
     n_repeats.times do
       lines.each do |s|
-        puts mask(s)
+        puts mask_with_case(s)
       end
     end
   end
